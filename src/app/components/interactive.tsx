39de8ccc35e405fcd61e8f96882ced4cfb508b13
SereneMatt <serene1990@gmail.com>
2024-12-03 01:29:46 +0100

    Ground work for wrong state

--- src/app/components/interactive.tsx
+++ src/app/components/interactive.tsx
@@ -4,15 +4,14 @@ import React, { useState } from 'react';
 
 import { servicePrefixMap } from '../../data';
 
-const service = 'Lambda';
-const prefix = 'AWS';
-
 const Interactive = () => {
-  const [serviceName, setServiceName] = useState(service);
+  const [serviceName, setServiceName] = useState('Lambda');
 
   const handleClick = (prefix: string, service: string) => {
     if (servicePrefixMap[service] === prefix) {
       setServiceName(`${prefix} ${service}`);
+    } else {
+      alert('Wrong!');
     }
   };
 
@@ -21,10 +20,16 @@ const Interactive = () => {
       <div>{serviceName}</div>
       <div className="flex gap-4 items-center flex-col sm:flex-row">
         <button
-          onClick={() => handleClick(prefix, service)}
+          onClick={() => handleClick('AWS', serviceName)}
           className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
         >
-          {prefix}
+          {'AWS'}
+        </button>
+        <button
+          onClick={() => handleClick('Amazon', serviceName)}
+          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+        >
+          {'Amazon'}
         </button>
       </div>
     </>

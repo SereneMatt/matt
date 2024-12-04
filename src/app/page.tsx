6d9c031bd304761a51d48cc495f92e9e95cd0b76
SereneMatt <serene1990@gmail.com>
2024-12-04 01:23:19 +0100

    Relearn how to write CSS without JS :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 import Image from 'next/image';
 
 import Interactive from './components/interactive';
+// .click-shake {
+//   background: #20bdbd;
+// }
 
+// .click-shake:active {
+//   animation: shake 0.5s ease-in-out;
+// }
+
+// @keyframes shake {
+//   0%, 100% {
+//       transform: translateX(0);
+//   }
+//   10%, 30%, 50%, 70%, 90% {
+//       transform: translateX(-5px);
+//   }
+//   20%, 40%, 60%, 80% {
+//       transform: translateX(5px);
+//   }
+// }
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">

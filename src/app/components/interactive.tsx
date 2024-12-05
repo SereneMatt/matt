151c89fa6cd106a7a40434981e2a372c954ec1cc
SereneMatt <serene1990@gmail.com>
2024-12-05 01:46:41 +0100

    Set CSS on click

--- src/app/components/interactive.tsx
+++ src/app/components/interactive.tsx
@@ -6,12 +6,13 @@ import { servicePrefixMap } from '../../data';
 
 const Interactive = () => {
   const [serviceName, setServiceName] = useState('Lambda');
+  const [jiggleClass, setJiggleClass] = useState(false);
 
   const handleClick = (prefix: string, service: string) => {
     if (servicePrefixMap[service] === prefix) {
       setServiceName(`${prefix} ${service}`);
     } else {
-      alert('Wrong!');
+      setJiggleClass(true);
     }
   };
 
@@ -27,7 +28,9 @@ const Interactive = () => {
         </button>
         <button
           onClick={() => handleClick('Amazon', serviceName)}
-          className="animate-[wiggle_1s_ease-in-out_infinite] rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+          className={`${
+            jiggleClass ? 'animate-[wiggle_1s_ease-in-out_infinite]' : ''
+          } rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44`}
         >
           {'Amazon'}
         </button>

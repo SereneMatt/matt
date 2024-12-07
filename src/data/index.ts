72cd5dfa28b3b6d6ec2d68df69f99e8e8448efc7
SereneMatt <serene1990@gmail.com>
2024-12-07 01:34:35 +0100

    React 19

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,12 @@
 export const servicePrefixMap: Record<string, string> = {
   Lambda: 'AWS',
-  'API Gateway': 'Amazon'
+  'API Gateway': 'Amazon',
+  SQS: 'Amazon',
+  ECS: 'Amazon',
+  DynamoDB: 'Amazon',
+  S3: 'Amazon',
+  CloudFront: 'Amazon',
+  Route53: 'Amazon',
+  Kinesis: 'Amazon',
+  ECR: 'Amazon'
 };

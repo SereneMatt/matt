035a8d2772ed2503af363b2893350c64f214e2d0
SereneMatt <serene1990@gmail.com>
2024-12-08 21:47:59 +0100

    Precie the game

--- src/app/components/interactive.tsx
+++ src/app/components/interactive.tsx
@@ -15,6 +15,7 @@ const Interactive = () => {
   const handleClick = (prefix: string, service: string) => {
     if (servicePrefixMap[service] === prefix) {
       setServiceName(`${prefix} ${service}`);
+      setServiceIndex(serviceIndex + 1);
       setRenderNext(true);
     } else {
       setJiggleClass(true);
@@ -22,29 +23,31 @@ const Interactive = () => {
   };
 
   const handleNextClick = () => {
-    setServiceIndex(serviceIndex + 1);
     setServiceName(services[serviceIndex]);
+    setRenderNext(false);
   };
 
   return (
     <>
       <div>{serviceName}</div>
-      <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <button
-          onClick={() => handleClick('AWS', serviceName)}
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-        >
-          {'AWS'}
-        </button>
-        <button
-          onClick={() => handleClick('Amazon', serviceName)}
-          className={`${
-            jiggleClass ? 'animate-[wiggle_1s_ease-in-out_infinite]' : ''
-          } rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44`}
-        >
-          {'Amazon'}
-        </button>
-      </div>
+      {!renderNext && (
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <button
+            onClick={() => handleClick('AWS', services[serviceIndex])}
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+          >
+            {'AWS'}
+          </button>
+          <button
+            onClick={() => handleClick('Amazon', services[serviceIndex])}
+            className={`${
+              jiggleClass ? 'animate-[wiggle_1s_ease-in-out_infinite]' : ''
+            } rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44`}
+          >
+            {'Amazon'}
+          </button>
+        </div>
+      )}
       {renderNext && (
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
